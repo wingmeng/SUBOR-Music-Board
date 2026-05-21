f68bd6915528d770107c937cf33df09153ab73be
Wing Meng <wingmeng@qq.com>
2026-05-21 08:39:08 +0800

    🎉 init:

---
+++ src/components/Board.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="board">
+    <h2 class="board-title">MUSIC BOARD</h2>
+    <div class="board-main">
+      <slot></slot>
+      0123456789 CDEFGAB cdefgab
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.board {
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 2 * 56px);
+  width: 32em;
+  margin: 56px auto;
+  box-sizing: border-box;
+  font-size: 20px;
+  color: #fff;
+  border: 17px solid;
+  border-image: url(../assets/border.svg) 17;
+  box-shadow: 0 0 0 2px #fff inset,
+        0 0 0 2px #fff,
+        0 0 0 56px #c33;
+}
+
+.board-title {
+  padding: 8px 2.6em;
+  margin: 10px auto;
+  font-weight: 400;
+  font-size: 120%;
+  border: 4px solid #36c;
+  border-radius: 6px;
+  corner-shape: bevel;
+}
+
+.board-main {
+  flex: 1;
+  min-height: 0;
+  word-break: break-all;
+  scrollbar-gutter: stable both-edges;
+  overflow: auto;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import '@fontsource/press-start-2p'
+import 'nes.css/css/nes-core.min.css'
+import './style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
